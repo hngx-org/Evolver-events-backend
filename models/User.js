0f8bf02ba2b34201521b32b37a1057601b4dbef7
sapphire <chidinmaakunwa@gmail.com>
2023-09-22 23:05:20 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -20,17 +20,6 @@ const User = db.define(
       type: DataTypes.TEXT,
       allowNull: true,
     },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-      onUpdate: DataTypes.NOW,
-    },
   },
   {
     timestamps: false,
