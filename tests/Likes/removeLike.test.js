ad7917d98b36c38ea06221f0cfa26f6df1f9121a
alibaba0010 <zakariyahali100@gmail.com>
2023-09-23 01:11:16 +0100

    adding removeLike

---
+++ tests/Likes/removeLike.test.js
@@ -0,0 +1,35 @@
+import removeLike from '../../controllers/likeController/methods/removeLike';
+import Likes from '../../models/Likes';
+
+jest.mock('../../models/Likes', ()=>{
+   destroy: jest.fn()
+})
+
+describe('remove like', () => {
+  it('should remove like from a comment', async () => {
+   const mockCommentId = '1';
+   const mockUserId = '2';
+
+   const req = {
+     params: {
+       commentId: mockCommentId,
+       userId: mockUserId,
+     },
+   };
+
+   const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await removeLike(req, res)
+    expect(Likes.destroy).toHaveBeenCalledWith({
+      where: {
+         comment_id: req.params.commentId,
+         user_id: req.params.userId,
+      },})
+  });
+  expect(res.status).toHaveBeenCalledWith(200);
+  expect(res.json).toHaveBeenCalledWith({ message: 'Like removed' });
+
+
+});
