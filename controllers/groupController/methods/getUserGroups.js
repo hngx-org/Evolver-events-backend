0514c82db16d13145576a796c41a62386b8da8fa
asahelcode <hoyx0101@gmail.com>
2023-09-24 12:19:45 +0100

    Add getAllGroups

--- controllers/groupController/methods/getUserGroups.js
+++ controllers/groupController/methods/getUserGroups.js
@@ -16,7 +16,7 @@ export default async function getUserGroups(req, res) {
         },
       ],
     }).then((user) => {
-      res.status(200).json(user);
+      res.status(200).json(user.groups);
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
