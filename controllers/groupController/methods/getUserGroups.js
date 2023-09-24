ae00668ef8a4f484316ad5b23c8609048bddbe1d
asahelcode <hoyx0101@gmail.com>
2023-09-24 12:11:24 +0100

    Fix: query user groups

--- controllers/groupController/methods/getUserGroups.js
+++ controllers/groupController/methods/getUserGroups.js
@@ -1,14 +1,23 @@
 import GroupMembership from "../../../models/GroupMembership.js";
+import Group from "../../../models/Group.js";
+import User from "../../../models/User.js";
 
 export default async function getUserGroups(req, res) {
   const { userId } = req.params;
 
   try {
-    const userGroups = await GroupMembership.findAll({
-      where: { user_id: userId },
+    User.findByPk(userId, {
+      include: [
+        {
+          model: Group,
+          through: {
+            model: GroupMembership,
+          },
+        },
+      ],
+    }).then((user) => {
+      res.status(200).json(user);
     });
-
-    res.status(200).json(userGroups);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
